435dbba060046ceb04667e55f3d4e913ed212ec8
Vladyslav Siminko <siminko.vlad23@gmail.com>
2020-12-29 16:45:09 +0200

    Create styledFooter.js

---
+++ src/styles/styledFooter.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const FooterStyled = styled.footer` 
+    text-align: center;
+    font-weight: bold;
+    font-size: 1.5rem;
+    padding: 10px;
+    background: #CADABA;
+    border-top: 3px solid black;
+`;
