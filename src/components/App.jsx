81d02f3eb30ad1105d527731e642456312fe92dd
Vladyslav Siminko <siminko.vlad23@gmail.com>
2020-12-29 17:01:08 +0200

    Create App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Container } from 'react-bootstrap';
+
+import { Header, Main, Footer, Loader } from './index';
+import { GlobalStyle } from '../styles/globalStyles';
+
+import { requestPhotos } from '../redux/fetchPhotos';
+
+export const App = () => {
+    const loading = useSelector(state => state.fetchPhotos.loading);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(requestPhotos());
+    }, []);
+
+    return (
+        <Container fluid>
+            <GlobalStyle />
+            <Header />
+            {loading ? <Loader /> : <Main />}
+            <Footer />
+        </Container>
+    );
+};
